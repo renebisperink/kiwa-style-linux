e1c2eaffa9324e88e0e7d73f80c77d45d56d5789
renebisperink <66679652+renebisperink@users.noreply.github.com>
2021-07-14 17:40:05 +0200

    Add files via upload

--- install_theme.sh
+++ install_theme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#A simple installscript for the kiwa theme. For now it only works on Kali (xfce) based systems. Some of the functions also work on Ubuntu/debian/parrot based OS'es
+#A simple installscript for the kiwa theme and the needed functionality. For now it only works on Ubuntu/Debian/Kali based systems.
 #uncomment the functions you need
 #@author Rene Bisperink
 #@version 0.3
@@ -8,10 +8,17 @@
 install_theme.sh () {
 	#update;
 	#addkiwauser;
-	#installkaliwslgui
-	configurexfce;
+	#installkaligui
+	#configurexfce;
 	#installpentest;
 	#installffdev;
+	#installmobilepentest;
+	#installiotre;
+	#installmobsf;
+	#cloneptrepos;
+	#addaliases;
+	
+	
 		
 }
 update () {
@@ -28,7 +35,16 @@ addkiwauser () {
 	
 }
 
-installkaliwslgui () {
+addaliases () {
+	echo "alias update='sudo apt update && sudo apt upgrade -y && cd /opt/ptf && sudo ./ptf --update-all -y'" >> ~/.zshrc
+	echo "alias lal='ls -al'" >> ~/.zshrc
+	echo "alias serviceunits='systemctl list-units --type=service'" >> ~/.zshrc
+	echo "alias status='systemctl status'" >> ~/.zshrc
+	echo "alias restart='systemctl restart'" >> ~/.zshrc
+	source ~/.zshrc
+}
+
+installkaligui () {
 	update;
 	echo "[*] Installing the kali-win-kex package to have a GUI on WSL"
 	sudo apt install kali-win-kex
@@ -37,7 +53,7 @@ installkaliwslgui () {
 configurexfce () {
 	configuretheme;
 
-	echo "[*] Setting the background for all monitors and workspaces"
+	echo "[*] Setting the background"
 	for b in $(xfconf-query --channel xfce4-desktop --list | grep last-image)
 	do
 		echo "Setting the background from $(pwd)/unnamed.jpg"
@@ -51,7 +67,7 @@ configurexfce () {
 		xfconf-query --channel xfce4-desktop --property $s --set 1
 	done
 	
-	echo "[*] Setting the color style for all monitors and workspaces"
+	echo "[*] Setting the color style"
 	for c in $(xfconf-query --channel xfce4-desktop --list | grep color-style)
 	do
 		echo "Setting the color style to centered on $c"
@@ -59,7 +75,7 @@ configurexfce () {
 	done
 
 	
-	echo "[*] Setting the wallpaper background color for all monitors and workspaces"
+	echo "[*] Setting the wallpaper background color"
 	for r in $(xfconf-query --channel xfce4-desktop --list | grep rgba1 )
 	do
 		echo "Setting the color background to white on $r"
@@ -72,7 +88,7 @@ configurexfce () {
 installpentest () {
 	update;
 	echo "[*] Installing pentest packages"
-	sudo apt install -y exiftool wine64 gdb wireshark wine seclists gobuster ftp php-curl python3-smb mingw-w64
+	sudo apt install -y exiftool wine64 gdb wireshark wine seclists gobuster ftp php-curl python3-smb mingw-w64 apt-transport-https git gdb gcc python3 cmake make curl p7zip-full p7zip-rar ghidra;
 	if [ -n "$(uname -a | grep Kali)"]; then
 		echo "[*] Installing the kali-linux-large package"
 		sudo apt install kali-linux-large -y
@@ -99,12 +115,94 @@ configuretheme () {
 	echo "[*] configuring the theme."
 	echo "[*] setting the icons to the Windows 10 icons from Kali"
 	xfconf-query -c xsettings -p /Net/IconThemeName -s Windows-10-Icons
-	echo "[*] setting the theme to Windows 10 from Kali"
+	echo "[*] setting the theme to Window 10 from Kali"
 	xfconf-query -c xsettings -p /Net/ThemeName -s Windows-10
 	echo "[*] setting the window manager to Window 10 from Kali"
 	xfconf-query -c xfwm4 -p /general/theme -s Windows-10
+	echo "[*] setting the taskbar color to black"
+	xfconf-query --channel xfce4-panel -p /panels/panel-1/background-rgba --create \
+        -t double -t double -t double -t double \
+        -s 0     -s 0     -s 0     -s 1.0
+	echo "[*] setting the panel / taskbar to the bottom of the screen"
+	xfconf-query --channel xfce4-panel -p /panels/panel-1/position -s 'p=8;x=960;y=1064'
+	echo "[*] locking the panel on the bottom"
+	xfconf-query --channel xfce4-panel -p /panels/panel-1/position-locked -s true
+	echo "[*] setting panel size to 36"
+	xfconf-query --channel xfce4-panel -p /panels/panel-1/size -s 36
+	echo "[*] setting icon size to 42"
+	xfconf-query --channel xfce4-desktop -p /desktop-icons/icon-size -s 42
 }
 
+installmobilepentest () {
+	update;
+	sudo apt install -y android-sdk android-sdk-platform-tools androguard dex2jar drozer
+}
+
+installmobsf (){
+	apt install docker.io
+	docker pull opensecurity/mobile-security-framework-mobsf
+	docker run -it -p 8000:8000 opensecurity-mobile-security-framework-mobsf:latest
+	echo "now open the browser on localhost:8000"
+}
+
+installiotre () {
+	sudo apt install binwalk openocd flashrom firmware-mod-kit killerbee hackrf ubertooth ubertooth-firmware gqrx gqrx-sdr multimon-ng dex2jar radare2 hackrf libhackrf-dev libhackrf0
+} 
+
+cloneptrepos () {
+	update;
+
+	if [ -n "$(uname -a | grep Kali)"]; then
+		sudo apt install -y seclists
+	else
+		sudo mkdir -p /usr/share/wordlists/
+		cd /usr/share/wordlists/
+		git clone https://github.com/danielmiessler/SecLists 
+	fi
+	cd /opt
+
+	git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite
+	git clone https://github.com/S3cur3Th1sSh1t/WinPwn/
+	git clone https://github.com/pentestmonkey/windows-privesc-check
+	git clone https://github.com/bitsadmin/wesng
+	git clone https://github.com/Anon-Exploiter/SUID3NUM
+	git clone https://github.com/DominicBreuker/pspy
+	git clone https://github.com/SecureAuthCorp/impacket
+	git clone https://github.com/M4ximuss/Powerless
+	git clone https://github.com/S3cur3Th1sSh1t/PowerSharpPack
+	git clone https://github.com/cobbr/SharpSploit
+	git clone https://github.com/boh/RedCsharp
+	git clone https://github.com/rebootuser/LinEnum
+	git clone https://github.com/411Hall/JAWS
+	git clone https://github.com/threat9/routersploit
+	git clone https://github.com/Ullaakut/cameradar
+`	
+}
+
+installazurecli () {
+	update;
+	sudo apt install ca-certificates curl apt-transport-https lsb-release gnupg
+	echo "[*] Adding the gpg signing key"
+	curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+	echo "[*] Adding the Azure CLI software repository"
+	AZ_REPO=$(lsb_release -cs)
+	echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+	sudo apt-get update
+	sudo apt-get install azure-cli
+	
+}
+
+installautorecon () {
+	update;
+	sudo apt install seclists curl enum4linux feroxbuster nbtscan nikto nmap onesixtyone oscanner smbclient smbmap smtp-user-enum snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf python3 python3-pip python3-venv
+	python3 -m pip install --user pipx
+	python3 -m pipx ensurepath
+	source ~/.zshrc
+	
+	echo 'alias sudo="sudo env \"PATH=$PATH\""' >> ~/.profile
+	pipx install git+https://github.com/Tib3rius/AutoRecon.git
+
+}
 
 
 installffdev () {
